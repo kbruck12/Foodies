e82e78aaeebd2b3e7222d1266404e23c60526570
ugam44 <mike_hodges44@yahoo.com>
2017-04-18 00:21:52 -0600

    changed store JSON data "company" to "name", fixed bootstrap link, added minor CSS

--- assets/js/store/stores.component.js
+++ assets/js/store/stores.component.js
@@ -7,13 +7,12 @@
         templateUrl: "/templates/store/stores.component.html",
         controller: function StoresCtrl ($http, $stateParams) {
             var $this = this;
-            console.dir($stateParams);
             $this.searchValue = $stateParams.searchValue || "";
             $this.search = function () {
                 var value = $this.searchValue.toLowerCase().trim();
                 //if (value !== "") {
                     $this.results = $this.stores.filter(function (store) {
-                        return (~store.company.toLowerCase().indexOf(value) ||
+                        return (~store.name.toLowerCase().indexOf(value) ||
                             ~store.city.toLowerCase().indexOf(value) ||
                             ~store.state.toLowerCase().indexOf(value) ||
                             ~store.zip.toString().indexOf(value) ||
